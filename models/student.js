32f80a0028eaf28eb995c1932cc473a0336181a2
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-05-22 10:03:58 +0700

    added hooks

--- models/student.js
+++ models/student.js
@@ -4,7 +4,13 @@ module.exports = (sequelize, DataTypes) => {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     email: DataTypes.STRING,
-  }, {});
+  }, {
+    hooks:{
+      beforeCreate:(student,options)=>{
+        student.first_name += ' sponsored by Aqua'
+      }
+    }
+  });
   Student.associate = function(models) {
     Student.hasMany(models.StudentsSubject)
   };
